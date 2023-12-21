15f39efd7572f82c53e522333212dc3c6ab33486
Martijn Stegeman <martijn@stgm.nl>
2023-12-21 20:22:03 +0100

    fxs

---
+++ tests/collectionsTest.py
@@ -0,0 +1,26 @@
+from checkpy import *
+from _basics import *
+from _static_analysis import *
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def emmeren():
+    """emmeren werkt correct"""
+    assert defines_function("emmeren")
+    assert getFunction("emmeren")([[1,2,3], [3,4], [4,5]]) == {3: [[1, 2, 3]], 2: [[3, 4], [4, 5]]}
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def maak_overzicht():
+    """maak_overzicht werkt correct"""
+    assert defines_function("maak_overzicht")
+    assert getFunction("maak_overzicht")([(2, 3, 0), (3, 0, 0)]) == {"cola": 5, "sinas": 3, "limonade": 0}
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def rits():
+    """rits werkt correct"""
+    assert defines_function("rits")
+    assert getFunction("rits")([1, 2, 3], [2, 4, 4]) == [(1, 2), (3, 4)]
+    assert getFunction("rits")([10, 30, 50, 70], [10, 20]) == [(10, 10)]
+    assert getFunction("rits")([5, 7, 9], []) == []
