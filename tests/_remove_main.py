6b6cc076f9b037574a094f3ada580865ef23f5e5
Martijn Stegeman <martijn@stgm.nl>
2023-12-23 12:22:33 +0100

    remover

--- tests/_remove_main.py
+++ tests/_remove_main.py
@@ -14,18 +14,36 @@ def remove_main(test):
             state = 0
             for line in file_contents:
                 if state == 0:
+                    # before first function: take everything, including first 'def' line
                     if line.startswith('def '):
                         state = 1
                     f.write(line)
                 elif state == 1:
+                    # function: take everything
+                    if line.startswith('"""'):
+                        f.write(line)
+                        state = 3
+                        continue
                     if not (line.strip() == '' or line.startswith(' ') or line.startswith("\t") or line.startswith("def ") or line.startswith("#") or line.find("->") >= 0):
+                        f.write(f"# {line}")
                         state = 2
                         continue
                     f.write(line)
                 elif state == 2:
-                    if line.startswith('def '):
+                    # after function: skip everything
+                    if line.startswith('#'):
+                        f.write(line)
+                    elif line.startswith('"""'):
+                        f.write(line)
+                        state = 3
+                    elif not line.startswith('def '):
+                        f.write(f"# {line}")
+                    else:
                         f.write(line)
                         state = 1
-                    else:
-                        f.write(f"# {line}")
+                elif state == 3:
+                    f.write(line)
+                    if line.endswith('"""'):
+                        state = 2
+
     test.test = testMethod
