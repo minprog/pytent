fc78c63f3ee000a6c0338ebf4d086db547546e9a
Martijn Stegeman <martijn@stgm.nl>
2024-12-03 09:38:10 +0100

    wc3/4 checks

---
+++ tests/wc3/formulesTest.py
@@ -0,0 +1,121 @@
+import sys
+import os
+
+# Add the parent directory to sys.path
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(parent_dir)
+
+from checkpy import *
+
+from _remove_main import *
+from _basics import *
+from _static_analysis import *
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def count_occurrences():
+    """count_occurrences werkt correct"""
+    assert defines_function("count_occurrences")
+    assert getFunction("count_occurrences")("hello", "l") == 2
+    assert getFunction("count_occurrences")("hello", "e") == 1
+    assert getFunction("count_occurrences")("hello", "x") == 0
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def has_O():
+    """has_O werkt correct"""
+    assert defines_function("has_O")
+    assert getFunction("has_O")("HELLO") == True
+    assert getFunction("has_O")("hello") == False
+    assert getFunction("has_O")("Ooops") == True
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def find():
+    """find werkt correct"""
+    assert defines_function("find")
+    assert getFunction("find")("hello", "e") == 1
+    assert getFunction("find")("hello", "x") == -1
+    assert getFunction("find")("hello", "l") == 2
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def has_up_and_down():
+    """has_up_and_down werkt correct"""
+    assert defines_function("has_up_and_down")
+    assert getFunction("has_up_and_down")("Hello") == True
+    assert getFunction("has_up_and_down")("HELLO") == False
+    assert getFunction("has_up_and_down")("hello") == False
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def multiply():
+    """multiply werkt correct"""
+    assert defines_function("multiply")
+    assert getFunction("multiply")("abc", 3) == "abcabcabc"
+    assert getFunction("multiply")("x", 0) == ""
+    assert getFunction("multiply")("", 5) == ""
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def reverse_string():
+    """reverse_string werkt correct"""
+    assert defines_function("reverse_string")
+    assert getFunction("reverse_string")("abcdef") == "fedcba"
+    assert getFunction("reverse_string")("") == ""
+    assert getFunction("reverse_string")("a") == "a"
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def leetspeak():
+    """leetspeak werkt correct"""
+    assert defines_function("leetspeak")
+    assert getFunction("leetspeak")("aleot") == "41307"
+    assert getFunction("leetspeak")("ALEOT") == "41307"
+    assert getFunction("leetspeak")("hello") == "h3110"
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def replace_char():
+    """replace_char werkt correct"""
+    assert defines_function("replace_char")
+    assert getFunction("replace_char")("hello", "l", "x") == "hexxo"
+    assert getFunction("replace_char")("hello", "e", "a") == "hallo"
+    assert getFunction("replace_char")("hello", "z", "x") == "hello"
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def to_lower():
+    """to_lower werkt correct"""
+    assert defines_function("to_lower")
+    assert getFunction("to_lower")("HeLLo") == "hello"
+    assert getFunction("to_lower")("HELLO") == "hello"
+    assert getFunction("to_lower")("hello") == "hello"
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def lstrip():
+    """lstrip werkt correct"""
+    assert defines_function("lstrip")
+    assert getFunction("lstrip")("   hello") == "hello"
+    assert getFunction("lstrip")("hello   ") == "hello   "
+    assert getFunction("lstrip")("") == ""
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def rstrip():
+    """rstrip werkt correct"""
+    assert defines_function("rstrip")
+    assert getFunction("rstrip")("hello world   ") == "hello world"
+    # assert getFunction("rstrip")("   hello   ") == "   hello"
+    assert getFunction("rstrip")("hello   ") == "hello"
+    assert getFunction("rstrip")("") == ""
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def split_in_two():
+    """split_in_two werkt correct"""
+    assert defines_function("split_in_two")
+    assert getFunction("split_in_two")("joey:22", ":") == ("joey", "22")
+    assert getFunction("split_in_two")("hello:world", ":") == ("hello", "world")
+    assert getFunction("split_in_two")("a:b", ":") == ("a", "b")
---
+++ tests/wc4/tentamenTest.py
@@ -0,0 +1,102 @@
+import sys
+import os
+
+# Add the parent directory to sys.path
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(parent_dir)
+
+from checkpy import *
+
+from _remove_main import *
+from _basics import *
+from _static_analysis import *
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def stretch():
+    """stretch werkt correct"""
+    assert defines_function("stretch")
+    assert getFunction("stretch")("abc") == "abbccc"
+    assert getFunction("stretch")("Hoi!") == "Hooiii!!!!"
+    assert getFunction("stretch")("") == ""
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def autocorrect():
+    """autocorrect werkt correct"""
+    assert defines_function("autocorrect")
+    assert getFunction("autocorrect")("Dit hier,, dit kan niet.") == "Dit hier, dit kan niet."
+    assert getFunction("autocorrect")("?!...!!") == "?!.!", "meerdere verschillende combinaties leestekens achter elkaar"
+    assert getFunction("autocorrect")("") == ""
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def str_diff():
+    """str_diff werkt correct"""
+    assert defines_function("str_diff")
+    assert getFunction("str_diff")("muikku", "kinkku") == 3
+    assert getFunction("str_diff")("aa", "aabb") == 2
+    assert getFunction("str_diff")("ab", "cdab") == 4
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def is_valid_email():
+    """is_valid_email werkt correct"""
+    assert defines_function("is_valid_email")
+    assert getFunction("is_valid_email")("test@example.com") == True
+    assert getFunction("is_valid_email")("invalid-email") == False
+    assert getFunction("is_valid_email")("test@.com") == False
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def longest_word_length():
+    """longest_word_length werkt correct"""
+    assert defines_function("longest_word_length")
+    assert getFunction("longest_word_length")("lahetetty joensuun hotellista") == 10
+    assert getFunction("longest_word_length")("abc def") == 3
+    assert getFunction("longest_word_length")("") == 0
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def encrypt():
+    """encrypt werkt correct"""
+    assert defines_function("encrypt")
+    assert getFunction("encrypt")("abc") == "zyx"
+    assert getFunction("encrypt")("xyz") == "cba"
+    assert getFunction("encrypt")("") == ""
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def remove_nth_word():
+    """remove_nth_word werkt correct"""
+    assert defines_function("remove_nth_word")
+    assert getFunction("remove_nth_word")("abc def geh", 3) in ["abc def", "abc def "]
+    assert getFunction("remove_nth_word")("one two three four", 2) in ["one three four", "one  three four"]
+    assert getFunction("remove_nth_word")("single", 1) in ["", " "]
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def remove_character():
+    """remove_character werkt correct"""
+    assert defines_function("remove_character")
+    assert getFunction("remove_character")("hello", "l") == "heo"
+    assert getFunction("remove_character")("banana", "a") == "bnn"
+    assert getFunction("remove_character")("", "x") == ""
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def find_longest_word():
+    """find_longest_word werkt correct"""
+    assert defines_function("find_longest_word")
+    assert getFunction("find_longest_word")("abc def gehijk lmn") == "gehijk"
+    assert getFunction("find_longest_word")("one two three") == "three"
+    assert getFunction("find_longest_word")("") == ""
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def count_substring():
+    """count_substring werkt correct"""
+    assert defines_function("count_substring")
+    assert getFunction("count_substring")("banana", "na") == 2
+    assert getFunction("count_substring")("aaaa", "aa") == 3
+    assert getFunction("count_substring")("hello", "x") == 0
