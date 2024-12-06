44647d81d91cfcf5c54eb0f22a7522d65996edf1
Martijn Stegeman <martijn@stgm.nl>
2024-12-06 11:04:52 +0100

    wcs

---
+++ tests/wc5/werkcollegeTest.py
@@ -0,0 +1,124 @@
+import sys
+import os
+
+# Add the parent directory to sys.path
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(parent_dir)
+
+from checkpy import *
+
+from _remove_main import *
+from _basics import *
+from _static_analysis import *
+
+from checkpy import *
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def check_forbidden():
+    """check of geen geavanceerde python gebruikt"""
+    assert in_code(ast.For)
+    assert not_in_code(ast.Set)
+    assert not_in_code(ast.Tuple)
+    assert not_in_code(ast.Dict)
+    assert not_has_stringmult()
+    assert not_has_stringmethods()
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def same_first_last():
+    """same_first_last werkt correct"""
+    assert defines_function("same_first_last")
+    assert getFunction("same_first_last")([1, 2, 3, 1]) == True
+    assert getFunction("same_first_last")([1, 2, 3]) == False
+    assert getFunction("same_first_last")([]) == False
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def is_longer():
+    """is_longer werkt correct"""
+    assert defines_function("is_longer")
+    assert getFunction("is_longer")([1, 2, 3], [1, 2]) == True
+    assert getFunction("is_longer")([], [1]) == False
+    assert getFunction("is_longer")([], []) == False
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def is_palindromish():
+    """is_palindromish werkt correct"""
+    assert defines_function("is_palindromish")
+    assert getFunction("is_palindromish")([1, 2, 3, 2, 1]) == True
+    assert getFunction("is_palindromish")([1, 2, 3]) == False
+    assert getFunction("is_palindromish")([]) == True
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def is_palindromish_eff():
+    """is_palindromish_eff werkt correct"""
+    assert defines_function("is_palindromish_eff")
+    assert getFunction("is_palindromish_eff")([1, 2, 3, 2, 1]) == True
+    assert getFunction("is_palindromish_eff")([1, 2, 3]) == False
+    assert getFunction("is_palindromish_eff")([]) == True
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def filter_even():
+    """filter_even werkt correct"""
+    assert defines_function("filter_even")
+    assert getFunction("filter_even")([1, 2, 3, 4]) == [2, 4]
+    assert getFunction("filter_even")([1, 3, 5]) == []
+    assert getFunction("filter_even")([]) == []
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def all_even():
+    """all_even werkt correct"""
+    assert defines_function("all_even")
+    assert getFunction("all_even")([2, 4, 6]) == True
+    assert getFunction("all_even")([2, 4, 5]) == False
+    assert getFunction("all_even")([]) == True
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def max_element():
+    """max_element werkt correct"""
+    assert defines_function("max_element")
+    assert getFunction("max_element")([1, 2, 3]) == 3
+    assert getFunction("max_element")([-1, -2, -3]) == -1
+    assert getFunction("max_element")([42]) == 42
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def element_exists():
+    """element_exists werkt correct"""
+    assert defines_function("element_exists")
+    assert getFunction("element_exists")([1, 2, 3], 2) == True
+    assert getFunction("element_exists")([1, 2, 3], 4) == False
+    assert getFunction("element_exists")([], 1) == False
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def count_occurrences():
+    """count_occurrences werkt correct"""
+    assert defines_function("count_occurrences")
+    assert getFunction("count_occurrences")([1, 2, 2, 3], 2) == 2
+    assert getFunction("count_occurrences")([1, 2, 3], 4) == 0
+    assert getFunction("count_occurrences")([], 1) == 0
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def remove_duplicates():
+    """remove_duplicates werkt correct"""
+    assert defines_function("remove_duplicates")
+    assert getFunction("remove_duplicates")([1, 2, 2, 3]) == [1, 2, 3]
+    assert getFunction("remove_duplicates")([1, 1, 1]) == [1]
+    assert getFunction("remove_duplicates")([]) == []
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def merge_sorted_lists():
+    """merge_sorted_lists werkt correct"""
+    assert defines_function("merge_sorted_lists")
+    assert getFunction("merge_sorted_lists")([1, 3, 5], [2, 4, 6]) == [1, 2, 3, 4, 5, 6]
+    assert getFunction("merge_sorted_lists")([], [2, 4]) == [2, 4]
+    assert getFunction("merge_sorted_lists")([], []) == []
---
+++ tests/wc6/collectionsTest.py
@@ -0,0 +1,104 @@
+import sys
+import os
+
+# Add the parent directory to sys.path
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(parent_dir)
+
+from checkpy import *
+
+from _remove_main import *
+from _basics import *
+from _static_analysis import *
+
+from checkpy import *
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def count():
+    """count werkt correct"""
+    assert defines_function("count")
+    assert getFunction("count")([1, 2, 3, 2]) == {1: 1, 2: 2, 3: 1}
+    assert getFunction("count")([4, 4, 4]) == {4: 3}
+    assert getFunction("count")([]) == {}
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def list_duplicates():
+    """list_duplicates werkt correct"""
+    assert defines_function("list_duplicates")
+    assert getFunction("list_duplicates")([1, 2, 3, 2, 3, 3]) == {2, 3}
+    assert getFunction("list_duplicates")([1, 2, 3]) == set()
+    assert getFunction("list_duplicates")([]) == set()
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def get_values():
+    """get_values werkt correct"""
+    assert defines_function("get_values")
+    assert getFunction("get_values")({"a": 1, "b": 2, "c": 3}) == [1, 2, 3]
+    assert getFunction("get_values")({}) == []
+    assert getFunction("get_values")({"x": 42}) == [42]
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def is_normal():
+    """is_normal werkt correct"""
+    assert defines_function("is_normal")
+    assert getFunction("is_normal")({"a": 0.5, "b": 0.5}) == True
+    assert getFunction("is_normal")({"a": 0.3, "b": 0.7, "c": 0.1}) == False
+    assert getFunction("is_normal")({}) == False
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def count_values():
+    """count_values werkt correct"""
+    assert defines_function("count_values")
+    assert getFunction("count_values")({"a": 1, "b": 2, "c": 1}) == 2
+    assert getFunction("count_values")({"a": 1, "b": 2, "c": 3}) == 3
+    assert getFunction("count_values")({}) == 0
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def reverse_dict():
+    """reverse_dict werkt correct"""
+    assert defines_function("reverse_dict")
+    assert getFunction("reverse_dict")({"a": 1, "b": 2}) == {1: "a", 2: "b"}
+    assert getFunction("reverse_dict")({}) == {}
+    assert getFunction("reverse_dict")({"x": 42}) == {42: "x"}
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def minst_voorkomende():
+    """minst_voorkomende werkt correct"""
+    assert defines_function("minst_voorkomende")
+    assert getFunction("minst_voorkomende")({"pepernoten": 100, "taaitaai": 20, "kruidnoten": 501}) == "taaitaai"
+    assert getFunction("minst_voorkomende")({"a": 1, "b": 1, "c": 2}) == "a"
+    assert getFunction("minst_voorkomende")({"x": 10}) == "x"
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def tel_dubbele():
+    """tel_dubbele werkt correct"""
+    assert defines_function("tel_dubbele")
+    assert getFunction("tel_dubbele")({"a": 1, "b": 2, "c": 1}) == 1
+    assert getFunction("tel_dubbele")({"a": 1, "b": 2, "c": 3}) == 0
+    assert getFunction("tel_dubbele")({}) == 0
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def dict_intersect():
+    """dict_intersect werkt correct"""
+    assert defines_function("dict_intersect")
+    assert getFunction("dict_intersect")({"a": 1, "b": 2}, {"a": 1, "c": 3}) == {"a": 1}
+    assert getFunction("dict_intersect")({"x": 42}, {"y": 42}) == {}
+    assert getFunction("dict_intersect")({}, {}) == {}
+
+@t.passed(no_syntax_error, hide=False)
+@t.test()
+def get_valuable_letters():
+    """get_valuable_letters werkt correct"""
+    assert defines_function("get_valuable_letters")
+    assert getFunction("get_valuable_letters")(4) == ["c", "f", "h", "j", "q", "u", "v", "w", "x", "y", "z"]
+    assert getFunction("get_valuable_letters")(8) == ["q", "x", "y"]
+    assert getFunction("get_valuable_letters")(11) == []
